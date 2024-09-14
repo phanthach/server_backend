4a72285842adf563f4b73eaabbabc83c794d2c28
Phan Tuấn Thạch - B20DCCN635 <98653656+phanthach@users.noreply.github.com>
2024-09-14 10:45:08 +0700

    Test

---
+++ Server/src/main/java/org/example/server/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.server.Repository;
+
+
+import org.example.server.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
+
